ffa48a53371a18599f0494f01e6364c5d6cf5279
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-11-28 18:38:20 -0200

    Adding escs.

---
+++ escs.sh
@@ -0,0 +1,12 @@
+##############################################################################
+# manage, use, java, github.
+cd ~/projects
+git clone git@github.com:iogf/projecteuler.scheme.git projecteuler.scheme-code
+
+# push, update, scheme.
+cd ~/projects/projecteuler.scheme-code
+git status
+git add *
+git commit -a
+git push
+
